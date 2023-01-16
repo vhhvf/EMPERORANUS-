c0ee07f5db0b01b014a321f2b605e1feb7875d13
vhhvf <122591347+vhhvf@users.noreply.github.com>
2023-01-16 19:33:55 +0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TI
 let chat = global.db.data.chats[m.chat]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`*[❗] مرحبا @${m.sender.split`@`[0]}*, لقد تم حظرك من استخدام بوت المرجو اتلكلم مع مالك لكي يزيل حظر و شكرا : https://wa.me/+393271166550?text=مرحبا`, false, { mentions: [m.sender] })
+await m.reply(`*[❗] مرحبا @${m.sender.split`@`[0]}*, لقد تم حظرك من استخدام بوت المرجو اتلكلم مع مالك لكي يزيل حظر و شكرا : https://wa.me/+967771160204?text=مرحبا`, false, { mentions: [m.sender] })
 await this.updateBlockStatus(m.chat, 'block')}
 return !1
 }
